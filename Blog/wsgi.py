eff8e1c12a80b6b171ecbfbb3464114b3a25565e
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2024-03-08 09:34:53 +0530

    Update wsgi.py

--- Blog/wsgi.py
+++ Blog/wsgi.py
@@ -15,7 +15,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blog.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
 # if not settings.DEBUG:
 #     try:
 #         application = get_wsgi_application()
